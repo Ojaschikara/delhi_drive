c439b84a9bb000474bcfb0169c92e0908d6b58a0
Ojaschikara <ojaschikara@email.com>
2024-09-20 13:31:30 +0530

    added

---
+++ scrolling/src/Components/Card.jsx
@@ -0,0 +1,11 @@
+const Card = ({ user }) => {
+    return (
+      <div className="card">
+        <img src={user.picture.large} alt={user.name.first} />
+        <h2>{`${user.name.first} ${user.name.last}`}</h2>
+        <p>Email: {user.email}</p>
+      </div>
+    );
+  };
+  
+  export default Card;
